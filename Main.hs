62047eab671ee994be2dce0ee42d999fdfdaac21
Sridhar Ratnakumar <srid@srid.ca>
2020-01-23 13:00:39 -0500

    Refactor

--- Main.hs
+++ Main.hs
@@ -11,8 +11,7 @@ module Main where
 
 import Clay ((?), Css, em, pc, pct, px, sym)
 import qualified Clay as C
-import Data.Aeson (FromJSON, fromJSON)
-import qualified Data.Aeson as Aeson
+import Control.Monad.Except (liftEither, withExceptT)
 import Data.Map (Map)
 import qualified Data.Map.Strict as Map
 import Data.Time
@@ -20,14 +19,27 @@ import Data.Time.Clock.POSIX
 import Development.Shake
 import Lucid
 import Path
-import Rib (MMark, Source)
+import Rib (Source)
 import qualified Rib
-import qualified Rib.Parser.MMark as MMark
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Prelude hiding (div, some)
 
 type Parser = Parsec Void Text
+
+data Log
+  = Log
+      { _log_time :: UTCTime,
+        _log_msg :: Text
+      }
+  deriving (Eq, Show)
+
+logParser :: Parser Log
+logParser = do
+  Just time <- fmap (posixSecondsToUTCTime . fromIntegral) . readMaybe @Int <$> some digitChar
+  -- TODO: parse msg further
+  msg <- toText <$> someTill anySingle eof
+  pure $ Log time msg
 
 data Channel
   = Channel
@@ -42,77 +54,41 @@ channelDirNameParser = do
   _ <- some (char '#')
   toText <$> someTill printChar (char '/')
 
-data Log
-  = Log
-      { _log_time :: UTCTime,
-        _log_msg :: Text
-      }
-  deriving (Eq, Show)
+mkChannel :: Text -> [Log] -> Channel
+mkChannel chName = Channel chName . groupLogs
+  where
+    groupLogs =
+      fmap (sortOn _log_time)
+        . Map.fromListWith (<>)
+        . fmap (\l -> (utctDay $ _log_time l, [l]))
 
-logParser :: Parser Log
-logParser = do
-  Just ts' :: Maybe Int <- fmap readMaybe $ some digitChar
-  let ts = posixSecondsToUTCTime $ fromIntegral ts'
-  msg <- toText <$> someTill anySingle eof
-  pure $ Log ts msg
-
-parseChannel :: (MonadIO m, MonadFail m) => Path b File -> m (Either Text Channel)
-parseChannel fp = do
+-- Parse ii's channel "out" file
+parseChannelFile :: (MonadIO m, MonadFail m) => Path b File -> m (Either Text Channel)
+parseChannelFile fp = runExceptT $ withExceptT (toText . errorBundlePretty) $ do
   let chDirName = toText $ toFilePath $ dirname $ parent fp
-  case parse channelDirNameParser "" chDirName of
-    Left e ->
-      pure $ Left $ toText $ errorBundlePretty e
-    Right chName -> do
-      logsRaw <- lines <$> readFileText (toFilePath fp)
-      logs <- flip traverse logsRaw $ \logRaw -> do
-        case parse logParser "" logRaw of
-          Left e ->
-            fail $ errorBundlePretty e
-          Right log ->
-            pure log
-      pure $ Right $ Channel chName
-        $ fmap (sortOn _log_time)
-        $ Map.fromListWith (<>)
-        $ (\l -> (utctDay $ _log_time l, [l])) <$> logs
+  chName <- liftEither $ parse channelDirNameParser "" chDirName
+  logsRaw <- lines <$> readFileText (toFilePath fp)
+  logs <- forM logsRaw $ liftEither . parse logParser ""
+  pure $ mkChannel chName logs
 
--- | This will be our type representing generated pages.
---
--- Each `Source` specifies the parser type to use. Rib provides `MMark` and
--- `Pandoc`; but you may define your own as well.
 data Page
   = Page_Index [Source Channel]
   | Page_Channel (Source Channel)
   | Page_ChannelDay Channel Day [Log]
 
--- | Main entry point to our generator.
---
--- `Rib.run` handles CLI arguments, and takes three parameters here.
---
--- 1. Directory `a`, from which static files will be read.
--- 2. Directory `b`, under which target files will be generated.
--- 3. Shake action to run.
---
--- In the shake build action you would expect to use the utility functions
--- provided by Rib to do the actual generation of your static site.
 main :: IO ()
 main = Rib.run [reldir|a|] [reldir|b|] generateSite
 
 -- | Shake action for generating the static site
 generateSite :: Action ()
 generateSite = do
-  -- Copy over the static files
   Rib.buildStaticFiles [[relfile|static/**|]]
-  -- Build individual sources, generating .html for each.
-  -- The function `buildHtmlMulti` takes the following arguments:
-  -- - Function that will parse the file (here we use mmark)
-  -- - File patterns to build
-  -- - Function that will generate the HTML (see below)
   chs <-
     Rib.forEvery [[relfile|#*/out|]] $ \fp -> do
-      chSrc <- Rib.buildHtml' fp parseChannel channelOutFile $ renderPage . Page_Channel
+      chSrc <- Rib.buildHtml' fp parseChannelFile channelOutFile $ renderPage . Page_Channel
       let ch = Rib.sourceVal chSrc
       forM_ (Map.toList $ _channel_logs ch) $ \(day, logs) -> do
-        dayFp <- liftIO $ parseRelFile $ toString $ _channel_name ch <> "/" <> show day <> ".html"
+        dayFp <- liftIO $ parseRelFile $ toString $ channelDayFile ch day
         Rib.writeHtml dayFp
           $ renderPage
           $ Page_ChannelDay ch day logs
@@ -125,7 +101,11 @@ generateSite = do
     channelOutFile =
       liftIO . parseRelFile . toString . (<> ".html") . _channel_name
 
--- | Define your site HTML here
+-- TODO: refactor `Source` so we can use it for source-less page slugs
+channelDayFile :: Channel -> Day -> Text
+channelDayFile ch day = _channel_name ch <> "/" <> show day <> ".html"
+
+-- | HTML view of our site.
 renderPage :: Page -> Html ()
 renderPage page = with html_ [lang_ "en"] $ do
   head_ $ do
@@ -154,8 +134,7 @@ renderPage page = with html_ [lang_ "en"] $ do
           h1_ $ toHtml $ _channel_name ch
           forM_ (Map.keys $ _channel_logs ch) $ \day -> do
             li_ $ do
-              -- TODO: refactor `Source` so we can use it for source-less page slugs
-              b_ $ with a_ [href_ $ "/" <> _channel_name ch <> "/" <> show day <> ".html"] $ do
+              b_ $ with a_ [href_ $ "/" <> channelDayFile ch day] $ do
                 toHtml $ show @Text day
         Page_ChannelDay ch day logs -> do
           with article_ [class_ "post"] $ do
@@ -186,20 +165,3 @@ pageStyle = "div#thesite" ? do
     C.marginTop $ em 1
     "b" ? C.fontSize (em 1.2)
     "p" ? sym C.margin (px 0)
-
--- | Metadata in our markdown sources
-data SrcMeta
-  = SrcMeta
-      { title :: Text,
-        -- | Description is optional, hence `Maybe`
-        description :: Maybe Text
-      }
-  deriving (Show, Eq, Generic, FromJSON)
-
--- | Get metadata from Markdown's YAML block
-getMeta :: Source MMark -> SrcMeta
-getMeta src = case MMark.projectYaml (Rib.sourceVal src) of
-  Nothing -> error "No YAML metadata"
-  Just val -> case fromJSON val of
-    Aeson.Error e -> error $ "JSON error: " <> toText e
-    Aeson.Success v -> v
